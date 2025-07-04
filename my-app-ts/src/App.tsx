8e1de7526c2c3b11c6eb400dd8bc214c87151b09
nandy <ehdwls6856@gmail.com>
2025-07-05 01:24:36 +0900

    feat: localStorage 에 인증값 추가

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [first, setFirst] = useState<string | null>(null);
+  const [second, setSecond] = useState<string | null>(null);
+  const [third, setThird] = useState<string | null>(null);
+
+  //todo: refactor
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const first = params.get('first');
+    const second = params.get('second');
+    const third = params.get('third');
+
+    if (first) {
+      localStorage.setItem('first', first);
+      setFirst(first);
+    } else {
+      const storedFirst = localStorage.getItem('first');
+      if (storedFirst) {
+        setFirst(storedFirst);
+      }
+    }
+
+    if (second) {
+      localStorage.setItem('second', second);
+      setSecond(second);
+    } else {
+      const storedSecond = localStorage.getItem('second');
+      if (storedSecond) {
+        setFirst(storedSecond);
+      }
+    }
+
+    if (third) {
+      localStorage.setItem('third', third);
+      setThird(third);
+    } else {
+      const storedThird = localStorage.getItem('third');
+      if (storedThird) {
+        setThird(storedThird);
+      }
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
