ad18b7713e6385679b1ee7871bb89c7ef0768a7d
DongJinNam <ehdwls6856@gmail.com>
2025-07-13 17:39:59 +0900

    fix: 피드백 반영(이미지 변경 && 메시지 변경)

--- solsolwind/src/QRCardScanner.tsx
+++ solsolwind/src/QRCardScanner.tsx
@@ -211,7 +211,7 @@ const QRCardScanner: React.FC = () => {
                 )}
                 {completionStatus[card.id] && (
                   <div className="mt-4 flex items-center justify-center">
-                    <img className="w-64 h-64" src={solsolLive} />
+                    <img className="w-64 h-64" src={solsolComplete} />
                   </div>
                 )}
               </div>
@@ -224,9 +224,10 @@ const QRCardScanner: React.FC = () => {
         {showMessage && (
           <div>
             <div className="mt-4 flex items-center justify-center">
-              <img className="w-128 h-128" src={solsolComplete} />
+              <img className="w-128 h-128" src={solsolLive} />
             </div>
             <div className="fixed top-20 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white px-6 py-3 rounded-lg shadow-lg animate-pulse z-50">
+              미션 클리어!
               상품 수령을 진행해주세요
             </div>            
           </div>
